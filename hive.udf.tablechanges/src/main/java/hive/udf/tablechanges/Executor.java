daeb094e8ffd54f1dce2c28b2967f8f222904906
Marcus Bittencourt <marcus.silva@softplan.com.br>
2019-04-29 13:53:20 -0300

    alteração do alvo de projeto para UDTF

--- hive.udf.tablechanges/src/main/java/hive/udf/tablechanges/Executor.java
+++ hive.udf.tablechanges/src/main/java/hive/udf/tablechanges/Executor.java
@@ -1,25 +1,18 @@
 package hive.udf.tablechanges;
 
-import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
-import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
-import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDTF;
 
-public class Executor extends GenericUDF {
+public class Executor extends GenericUDTF {
 
     @Override
-    public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
-        return null;
+    public void process(Object[] args) throws HiveException {
+
     }
 
     @Override
-    public Object evaluate(DeferredObject[] arguments) throws HiveException {
-        return null;
-    }
+    public void close() throws HiveException {
 
-    @Override
-    public String getDisplayString(String[] children) {
-        return null;
     }
 
 }
